5e2189e790004bb7d178dd9b9d169d9d65a82d93
Kai Mareving <karska.joanna@gmail.com>
2021-10-28 14:51:59 +0200

    init hbs v.0.0

--- server.js
+++ server.js
@@ -1,35 +1,36 @@
 const express = require('express');
 const path = require('path');
-let isLogged = false;
+const hbs = require('express-handlebars');
 
 const app = express();
+app.engine('.hbs', hbs());
+app.set('view engine', '.hbs');
 
 app.use(express.static(path.join(__dirname, '/public')));
 
-app.use((req, res, next) => {
-  res.show = (name) => {
-    res.sendFile(path.join(__dirname, `/views/${name}`));
-  };
-  next();
-});
-
 /* Specific endpoints */
+let isLogged = false;
+
 app.use('/user*', (req, res, next) => {
   if(isLogged) next();
-  else res.show('forbidden.html');
+  else res.render('forbidden', { layout: false });
+});
+
+app.get('/hello/:name', (req, res) => {
+  res.render('hello', { layout: false, name: req.params.name });
 });
 
 app.get(['/', '/home'], (req, res) => {
-  res.show('home.html');
+  res.render('home', { layout: false });
 });
 
 app.get('/about', (req, res) => {
-  res.show('about.html');
+  res.render('about', { layout: false });
 });
 
 app.use((req, res) => {
   //or res.status(404).send('404 not found...');
-  res.status(404).show('404.html');
+  res.status(404).render('404', { layout: false });
 });
 
 app.listen(8000, () => {
