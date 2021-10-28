af74f45325b46eef039f9da1b201a8a11076da2b
Kai Mareving <karska.joanna@gmail.com>
2021-10-29 01:14:44 +0200

    27.3 dark layout

--- server.js
+++ server.js
@@ -1,36 +1,38 @@
 const express = require('express');
 const path = require('path');
 const hbs = require('express-handlebars');
+let isLogged = false;
 
 const app = express();
 app.engine('.hbs', hbs());
+//or app.engine('.hbs', hbs({ extname: 'hbs', layoutsDir: './views/layouts', defaultLayout: 'main' }));
 app.set('view engine', '.hbs');
 
 app.use(express.static(path.join(__dirname, '/public')));
 
-/* Specific endpoints */
-let isLogged = false;
 
+/* Specific endpoints */
 app.use('/user*', (req, res, next) => {
   if(isLogged) next();
-  else res.render('forbidden', { layout: false });
+  else res.render('forbidden');
 });
 
-app.get('/hello/:name', (req, res) => {
-  res.render('hello', { layout: false, name: req.params.name });
-});
 
 app.get(['/', '/home'], (req, res) => {
-  res.render('home', { layout: false });
+  res.render('home');
 });
 
 app.get('/about', (req, res) => {
-  res.render('about', { layout: false });
+  res.render('about', { layout: 'dark' });
+});
+
+app.get('/hello/:name', (req, res) => {
+  res.render('hello', { name: req.params.name });
 });
 
 app.use((req, res) => {
   //or res.status(404).send('404 not found...');
-  res.status(404).render('404', { layout: false });
+  res.status(404).render('404');
 });
 
 app.listen(8000, () => {
