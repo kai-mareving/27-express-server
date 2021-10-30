5b3ed08b382625eb56a87a05b659983e3dd51beb
Kai Mareving <karska.joanna@gmail.com>
2021-10-30 17:21:54 +0200

    27.4 v.0.1

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const path = require('path');
 const hbs = require('express-handlebars');
+const multer  = require('multer');
+const upload = multer({ dest: './public/data/uploads/' });
 let isLogged = false;
 
 const app = express();
@@ -9,7 +11,8 @@ app.engine('.hbs', hbs());
 app.set('view engine', '.hbs');
 
 app.use(express.static(path.join(__dirname, '/public')));
-
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 /* Specific endpoints */
 app.use('/user*', (req, res, next) => {
@@ -28,6 +31,20 @@ app.get('/about', (req, res) => {
 
 app.get('/hello/:name', (req, res) => {
   res.render('hello', { name: req.params.name });
+});
+
+app.get('/contact', (req, res) => {
+  res.render('contact', { layout: 'dark' });
+});
+
+app.post('/contact/send-message', upload.single('file'), function (req, res) {
+  const { author, sender, title, message } = req.body;
+
+  if (author && sender && title && message && req.file) {
+    res.render('contact', { layout: 'dark', isSent: true, file: req.file.originalname });
+  } else {
+    res.render('contact', { layout: 'dark', isError: true });
+  }
 });
 
 app.use((req, res) => {
